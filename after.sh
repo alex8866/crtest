1b1adef821e0b5453916969ccc943919d6a4a45f
lkong <lkong@tencent.com>
2017-11-19 19:41:53 +0800

    add after.sh

---
+++ after.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: after.sh
+# Description: after.sh
+# Author: lkong
+# mail: lkong@tencent.com
+# Created Time: 2017-11-19 19:41:33
+#########################################################################
+#!/bin/bash
+
+echo after > /tmp/after
